ce56811d352edf106fabe83d0eefddd579809c98
hmcoding <32525204+hmcoding@users.noreply.github.com>
2022-05-22 12:10:21 -0400

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-//messageOne.textContent = 'From JS'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -12,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location)).then((response) => {
+    fetch('/weather?address=' + encodeURIComponent(location)).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
